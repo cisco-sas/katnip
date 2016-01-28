4221174dccf0a0d9500cbf08d469deaaca556d6c
bsharet <bsharet@cisco.com>
2016-01-28 17:58:24 +0200

    USB templates - Dynamic fields
    - Added dynamic fields to the Configuration and Interface USB templates.
    - Began adding USB HID templates, still more work to do there.

--- katnip/templates/usb.py
+++ katnip/templates/usb.py
@@ -24,6 +24,7 @@ http://www.usb.org/developers/docs/usb20_docs/usb_20_042814.zip
 '''
 
 from kitty.model import *
+from katnip.legos.dynamic import DynamicInt
 
 
 class _StandardRequestCodes:
@@ -60,6 +61,8 @@ class _DescriptorTypes:
     HUB = 0x29
     CS_INTERFACE = 0x24  # usbcdc11.pdf table 24
     CS_ENDPOINT = 0x25  # usbcdc11.pdf table 24
+    DEVICE_HID = 0x21
+    REPORT = 0x22
 
 
 class _StandardFeatureSelector:
@@ -74,10 +77,11 @@ class Descriptor(Template):
     USB descriptor template.
     '''
 
-    def __init__(self, name, fields):
+    def __init__(self, name, descriptor_type, fields, fuzz_type=True):
         if isinstance(fields, BaseField):
             fields = [fields]
         fields.insert(0, SizeInBytes(name='bLength', sized_field=self, length=8, fuzzable=True))
+        fields.insert(1, UInt8(name='bDescriptorType', value=descriptor_type, fuzzable=fuzz_type))
         super(Descriptor, self).__init__(name=name, fields=fields)
 
 
@@ -101,16 +105,16 @@ class SizedPt(Container):
 # Section 9.6.1, page 261
 device_descriptor = Descriptor(
     name='device_descriptor',
+    descriptor_type=_DescriptorTypes.DEVICE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.DEVICE, fuzzable=True),
-        UInt16(name='bcdUSB', value=0x0100),  # USB 2.0 is reported as 0x0200, USB 1.1 as 0x0110 and USB 1.0 as 0x0100
+        LE16(name='bcdUSB', value=0x0100),  # USB 2.0 is reported as 0x0200, USB 1.1 as 0x0110 and USB 1.0 as 0x0100
         UInt8(name='bDeviceClass', value=0),
         UInt8(name='bDevuceSubClass', value=0),
         UInt8(name='bDeviceProtocol', value=0),
         UInt8(name='bMaxPacketSize', value=64),  # valid sizes: 8,16,32,64
-        UInt16(name='idVendor', value=0),
-        UInt16(name='idProduct', value=0),
-        UInt16(name='bcdDevice', value=0),
+        LE16(name='idVendor', value=0),
+        LE16(name='idProduct', value=0),
+        LE16(name='bcdDevice', value=0),
         UInt8(name='iManufacturer', value=0),
         UInt8(name='iProduct', value=0),
         UInt8(name='iSerialNumber', value=0),
@@ -121,9 +125,9 @@ device_descriptor = Descriptor(
 # Section 9.6.2, page 264
 device_qualifier_descriptor = Descriptor(
     name='device_qualifier_descriptor',
+    descriptor_type=_DescriptorTypes.DEVICE_QUALIFIER,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.DEVICE_QUALIFIER, fuzzable=True),
-        UInt16(name='bcdUSB', value=0x0100),  # USB 2.0 is reported as 0x0200, USB 1.1 as 0x0110 and USB 1.0 as 0x0100
+        LE16(name='bcdUSB', value=0x0100),  # USB 2.0 is reported as 0x0200, USB 1.1 as 0x0110 and USB 1.0 as 0x0100
         UInt8(name='bDeviceClass', value=0),
         UInt8(name='bDevuceSubClass', value=0),
         UInt8(name='bDeviceProtocol', value=0),
@@ -132,19 +136,26 @@ device_qualifier_descriptor = Descriptor(
         UInt8(name='bReserved', value=0)
     ])
 
-
+'''
+'wTotalLength': struct.pack('<H', wTotalLength),
+'bNumInterfaces': bytes(bNumInterfaces),
+'bConfigurationValue': bytes(self.configuration_index),
+'iConfiguration': bytes(self.configuration_string_index),
+'bmAttributes': bytes(self.attributes),
+'bMaxPower': bytes(self.max_power)
+'''
 # Configuration descriptor
 # Section 9.6.3, page 265
 configuration_descriptor = Descriptor(
     name='configuration_descriptor',
+    descriptor_type=_DescriptorTypes.CONFIGURATION,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CONFIGURATION, fuzzable=True),
-        UInt16(name='wTotalLength', value=9),  # TODO: use sizer ?
-        UInt8(name='bNumInterfaces', value=1),
-        UInt8(name='bConfigurationValue', value=1),
-        UInt8(name='iConfiguration', value=0),
-        BitField(name='bmAttributes', value=0, length=8),
-        UInt8(name='bMaxPower', value=1)
+        DynamicInt('wTotalLength', LE16(value=9)),
+        DynamicInt('bNumInterfaces', UInt8(value=1)),
+        DynamicInt('bConfigurationValue', UInt8(value=1)),
+        DynamicInt('iConfiguration', UInt8(value=0)),
+        DynamicInt('bmAttributes', BitField(value=0, length=8)),
+        DynamicInt('bMaxPower', UInt8(value=1)),
     ])
 
 
@@ -152,9 +163,9 @@ configuration_descriptor = Descriptor(
 # Section 9.6.4, page 267
 other_speed_configuration_descriptor = Descriptor(
     name='other_speed_configuration_descriptor',
+    descriptor_type=_DescriptorTypes.OTHER_SPEED_CONFIGURATION,
     fields=[
-        UInt8(name='bDescriptorType', value=_DescriptorTypes.OTHER_SPEED_CONFIGURATION, fuzzable=True),
-        UInt16(name='wTotalLength', value=0xffff),  # TODO: real default size
+        LE16(name='wTotalLength', value=0xffff),  # TODO: real default size
         UInt8(name='bNumInterfaces', value=0xff),  # TODO: real default size
         UInt8(name='bConfigurationValue', value=0xff),  # TODO: real default size
         UInt8(name='iConfiguration', value=0xff),
@@ -167,15 +178,15 @@ other_speed_configuration_descriptor = Descriptor(
 # Section 9.6.5, page 267
 interface_descriptor = Descriptor(
     name='interface_descriptor',
+    descriptor_type=_DescriptorTypes.INTERFACE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.INTERFACE, fuzzable=True),
-        UInt8(name='bInterfaceNumber', value=0),
-        UInt8(name='bAlternateSetting', value=0),
-        UInt8(name='bNumEndpoints', value=0x1),
-        UInt8(name='bInterfaceClass', value=0x08),  # 0x08 mass storage
-        UInt8(name='bInterfaceSubClass', value=0x06),  # 0x06 mass storage
-        UInt8(name='bInterfaceProtocol', value=0x50),  # 0x50 mass storage
-        UInt8(name='iInterface', value=0)
+        DynamicInt('bInterfaceNumber', UInt8(value=0)),
+        DynamicInt('bAlternateSetting', UInt8(value=0)),
+        DynamicInt('bNumEndpoints', UInt8(value=0x1)),
+        DynamicInt('bInterfaceClass', UInt8(value=0x08)),  # 0x08 mass storage
+        DynamicInt('bInterfaceSubClass', UInt8(value=0x06)),  # 0x06 mass storage
+        DynamicInt('bInterfaceProtocol', UInt8(value=0x50)),  # 0x50 mass storage
+        DynamicInt('iInterface', UInt8(value=0)),
     ])
 
 
@@ -183,11 +194,11 @@ interface_descriptor = Descriptor(
 # Section 9.6.6, page 269
 endpoint_descriptor = Descriptor(
     name='endpoint_descriptor',
+    descriptor_type=_DescriptorTypes.ENDPOINT,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.ENDPOINT, fuzzable=True),
         UInt8(name='bEndpointAddress', value=0),
         BitField(name='bmAttributes', value=0, length=8),
-        UInt16(name='wMaxPacketSize', value=65535),
+        LE16(name='wMaxPacketSize', value=65535),
         UInt8(name='bInterval', value=0)
     ])
 
@@ -196,23 +207,23 @@ endpoint_descriptor = Descriptor(
 # Section 9.6.7, page 273
 string_descriptor = Descriptor(
     name='string_descriptor',
+    descriptor_type=_DescriptorTypes.STRING,
     fields=[
-        UInt8(name='bDescriptorType', value=_DescriptorTypes.STRING, fuzzable=True),
         String(name='bString', value='hello_kitty', encoder=StrEncodeEncoder('utf_16_le'), max_size=254/2)
     ])
 
 
 string_descriptor_zero = Descriptor(
     name='string_descriptor_zero',
+    descriptor_type=_DescriptorTypes.STRING,
     fields=[
-        UInt8(name='bDescriptorType', value=_DescriptorTypes.STRING, fuzzable=True),
         RandomBytes(name='lang_id', min_length=0, max_length=253, step=3, value='\x04\x09')
     ])
 
 hub_descriptor = Descriptor(
     name='hub_descriptor',
+    descriptor_type=_DescriptorTypes.HUB,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.HUB, fuzzable=True),
         UInt8(name='bNbrPorts', value=4),
         BitField(name='wHubCharacteristics', value=0xe000, length=16),
         UInt8(name='bPwrOn2PwrGood', value=0x32),
@@ -250,10 +261,10 @@ SCSI_op_code_0x03 = Template(
         UInt8(name='ILI', value=0x00),
         UInt8(name='EOM', value=0x00),
         UInt8(name='FILEMARK', value=0x00),
-        UInt32(name='Information', value=0x00),
+        BE32(name='Information', value=0x00),
         SizedPt(name='Additional_Sense_data',
                 fields=[
-                    UInt32(name='CmdSpecificInfo', value=0x00),
+                    BE32(name='CmdSpecificInfo', value=0x00),
                     UInt8(name='ASC', value=0x00),
                     UInt8(name='ASCQ', value=0x00),
                     UInt8(name='FRUC', value=0x00),
@@ -285,13 +296,13 @@ SCSI_op_code_0x12 = Template(
 
 
 # Mode Sence - FuzzableUSBMassStorageInterface
-SCSI_op_code_0x1a_or_0x5a = Descriptor(
+SCSI_op_code_0x1a_or_0x5a = Template(
     name='SCSI_op_code_0x1a_or_0x5a',
     fields=[
+        SizeInBytes(name='bLength', sized_field='SCSI_op_code_0x1a_or_0x5a', length=8, fuzzable=True),
         UInt8(name='MediumType', value=0x00),
         UInt8(name='Device_Specific_Param', value=0x00),
-        SizedPt(name='Mode_Parameter_Container',
-                fields=RandomBytes(name='Mode_Parameter', min_length=0, max_length=4, value='\x1c'))
+        SizedPt(name='Mode_Parameter_Container', fields=RandomBytes(name='Mode_Parameter', min_length=0, max_length=4, value='\x1c'))
     ])
 
 
@@ -299,10 +310,10 @@ SCSI_op_code_0x1a_or_0x5a = Descriptor(
 SCSI_op_code_0x23 = Template(
     name='SCSI_op_code_0x23',
     fields=[
-        UInt32(name='capacity_list_length', value=0x8),
-        UInt32(name='num_of_blocks', value=0x1000),
-        UInt16(name='descriptor_code', value=0x1000),
-        UInt16(name='block_length', value=0x0200)
+        BE32(name='capacity_list_length', value=0x8),
+        BE32(name='num_of_blocks', value=0x1000),
+        BE16(name='descriptor_code', value=0x1000),
+        BE16(name='block_length', value=0x0200)
     ])
 
 
@@ -310,8 +321,8 @@ SCSI_op_code_0x23 = Template(
 SCSI_op_code_0x25 = Template(
     name='SCSI_op_code_0x25',
     fields=[
-        UInt32(name='NumBlocks', value=0x4fff),
-        UInt32(name='BlockLen', value=0x200)
+        BE32(name='NumBlocks', value=0x4fff),
+        BE32(name='BlockLen', value=0x200)
     ])
 
 
@@ -352,29 +363,29 @@ class _CDC_DescriptorSubTypes:  # CDC Functional Descriptors
 
 cdc_header_functional_descriptor = Descriptor(
     name='cdc_header_functional_descriptor',
+    descriptor_type=_DescriptorTypes.CS_INTERFACE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CS_INTERFACE, fuzzable=True),
         UInt8(name='bDesciptorSubType', value=_CDC_DescriptorSubTypes.HEADER_FUNCTIONAL),
-        UInt16(name='bcdCDC', value=0xffff)
+        LE16(name='bcdCDC', value=0xffff)
     ])
 
 
 cdc_call_management_functional_descriptor = Descriptor(
     name='cdc_call_management_functional_descriptor',
+    descriptor_type=_DescriptorTypes.CS_INTERFACE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CS_INTERFACE, fuzzable=True),
         UInt8(name='bDesciptorSubType', value=_CDC_DescriptorSubTypes.CALL_MANAGMENT),
         BitField(name='bmCapabilities', value=0, length=8),
         UInt8(name='bDataInterface', value=0)
     ])
 
 
-## TODO: Missing descriptors for subtypes 3,4,5
+# TODO: Missing descriptors for subtypes 3,4,5
 
 cdc_abstract_control_management_functional_descriptor = Descriptor(
     name='cdc_abstract_control_management_functional_descriptor',
+    descriptor_type=_DescriptorTypes.CS_INTERFACE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CS_INTERFACE, fuzzable=True),
         UInt8(name='bDesciptorSubType', value=_CDC_DescriptorSubTypes.ABSTRACT_CONTROL_MANAGEMENT),
         BitField(name='bmCapabilities', value=0, length=8)
     ])
@@ -382,25 +393,25 @@ cdc_abstract_control_management_functional_descriptor = Descriptor(
 
 cdc_union_functional_descriptor = Descriptor(
     name='cdc_union_functional_descriptor',
+    descriptor_type=_DescriptorTypes.CS_INTERFACE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CS_INTERFACE, fuzzable=True),
         UInt8(name='bDesciptorSubType', value=_CDC_DescriptorSubTypes.UNION_FUNCTIONAL),
         UInt8(name='bMasterInterface', value=0),
         Repeat(UInt8(name='bSlaveInterfaceX', value=0), 0, 251)
     ])
 
 
-## TODO: Missing descriptors 7,8,9,10,11,12,13,14
+# TODO: Missing descriptors 7,8,9,10,11,12,13,14
 
 cdc_ethernet_networking_functional_descriptor = Descriptor(
     name='cdc_ethernet_networking_functional_descriptor',
+    descriptor_type=_DescriptorTypes.CS_INTERFACE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CS_INTERFACE, fuzzable=True),
         UInt8(name='bDesciptorSubType', value=_CDC_DescriptorSubTypes.ETHERNET_NETWORKING),
         UInt8(name='iMACAddress', value=0),
         BitField(name='bmEthernetStatistics', value=0xffffffff, length=32),
-        UInt16(name='wMaxSegmentSize', value=1514),
-        UInt16(name='wNumberMCFilters', value=0),
+        LE16(name='wMaxSegmentSize', value=1514),
+        LE16(name='wNumberMCFilters', value=0),
         UInt8(name='bNumberPowerFilters', value=0)
     ])
 
@@ -435,26 +446,26 @@ class _AS_DescriptorSubTypes:  # AS Interface Descriptor Subtype
 
 adc_header_descriptor = Descriptor(
     name='adc_header_descriptor',
+    descriptor_type=_DescriptorTypes.CS_INTERFACE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CS_INTERFACE, fuzzable=True),
         UInt8(name='bDesciptorSubType', value=_AC_DescriptorSubTypes.HEADER),
-        UInt16(name='bcdADC', value=0x0100),
-        UInt16(name='wTotalLength', value=0x1e),
+        LE16(name='bcdADC', value=0x0100),
+        LE16(name='wTotalLength', value=0x1e),
         UInt8(name='bInCollection', value=0x1),
         Repeat(UInt8(name='baInterfaceNrX', value=1), 0, 247)
     ])
 
 
 adc_input_terminal_descriptor = Descriptor(
+    descriptor_type=_DescriptorTypes.CS_INTERFACE,
     name='adc_input_terminal_descriptor',
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CS_INTERFACE, fuzzable=True),
         UInt8(name='bDesciptorSubType', value=_AC_DescriptorSubTypes.INPUT_TERMINAL),
         UInt8(name='bTerminalID', value=0x00),
-        UInt16(name='wTerminalType', value=0x0206),  # termt10.pdf table 2-2
+        LE16(name='wTerminalType', value=0x0206),  # termt10.pdf table 2-2
         UInt8(name='bAssocTerminal', value=0x00),
         UInt8(name='bNrChannels', value=0x01),
-        UInt16(name='wChannelConfig', value=0x0101),
+        LE16(name='wChannelConfig', value=0x0101),
         UInt8(name='iChannelNames', value=0x00),
         UInt8(name='iTerminal', value=0x00)
     ])
@@ -462,11 +473,11 @@ adc_input_terminal_descriptor = Descriptor(
 
 adc_output_terminal_descriptor = Descriptor(
     name='adc_output_terminal_descriptor',
+    descriptor_type=_DescriptorTypes.CS_INTERFACE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CS_INTERFACE, fuzzable=True),
         UInt8(name='bDesciptorSubType', value=_AC_DescriptorSubTypes.OUTPUT_TERMINAL),
         UInt8(name='bTerminalID', value=0x00),
-        UInt16(name='wTerminalType', value=0x0307),  # termt10.pdf table 2-3
+        LE16(name='wTerminalType', value=0x0307),  # termt10.pdf table 2-3
         UInt8(name='bAssocTerminal', value=0x00),
         UInt8(name='bSourceID', value=0x01),
         UInt8(name='iTerminal', value=0x00)
@@ -477,8 +488,8 @@ adc_output_terminal_descriptor = Descriptor(
 # Table 4-7
 adc_feature_unit_descriptor = Descriptor(
     name='adc_feature_unit_descriptor',
+    descriptor_type=_DescriptorTypes.CS_INTERFACE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CS_INTERFACE, fuzzable=True),
         UInt8(name='bDesciptorSubType', value=_AC_DescriptorSubTypes.FEATURE_UNIT),
         UInt8(name='bUnitID', value=0x00),
         UInt8(name='bSourceID', value=0x00),
@@ -491,19 +502,19 @@ adc_feature_unit_descriptor = Descriptor(
 # Table 4-19
 adc_as_interface_descriptor = Descriptor(
     name='adc_as_interface_descriptor',
+    descriptor_type=_DescriptorTypes.CS_INTERFACE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CS_INTERFACE, fuzzable=True),
         UInt8(name='bDesciptorSubType', value=_AS_DescriptorSubTypes.AS_GENERAL),
         UInt8(name='bTerminalLink', value=0x00),
         UInt8(name='bDelay', value=0x00),
-        UInt16(name='wFormatTag', value=0x0001)
+        LE16(name='wFormatTag', value=0x0001)
     ])
 
 
 adc_as_format_type_descriptor = Descriptor(
     name='adc_as_format_type_descriptor',
+    descriptor_type=_DescriptorTypes.CS_INTERFACE,
     fields=[
-        UInt8(name='bDesciptorType', value=_DescriptorTypes.CS_INTERFACE, fuzzable=True),
         UInt8(name='bDesciptorSubType', value=_AS_DescriptorSubTypes.FORMAT_TYPE),
         UInt8(name='bFormatType', value=0x01),
         UInt8(name='bNrChannels', value=0x01),
@@ -514,11 +525,51 @@ adc_as_format_type_descriptor = Descriptor(
     ])
 
 
+###################################################
+#              HID Class Templates                #
+###################################################
+
+hid_configuration_descriptor = Descriptor(
+    name='hid_configuration_descriptor',
+    descriptor_type=_DescriptorTypes.CONFIGURATION,
+    fields=[
+        LE16(name='wTotalLength', value=9),  # TODO: use sizer ?
+        UInt8(name='bNumInterfaces', value=1),
+        UInt8(name='bConfigurationValue', value=1),
+        UInt8(name='iConfiguration', value=0),
+        BitField(name='bmAttributes', value=0xa0, length=8),
+        UInt8(name='bMaxPower', value=0x32)
+    ])
+
+hid_interface_descriptor = Descriptor(
+    name='hid_interface_descriptor',
+    descriptor_type=_DescriptorTypes.INTERFACE,
+    fields=[
+        UInt8(name='bInterfaceNumber', value=0),
+        UInt8(name='bAlternateSetting', value=0),
+        UInt8(name='bNumEndpoints', value=0x2),
+        UInt8(name='bInterfaceClass', value=0x03),  # 0x08 HID
+        UInt8(name='bInterfaceSubClass', value=0x00),
+        UInt8(name='bInterfaceProtocol', value=0x00),
+        UInt8(name='iInterface', value=0)
+    ])
+
+hid_hid_descriptor = Descriptor(
+    name='hid_hid_descriptor',
+    descriptor_type=_DescriptorTypes.DEVICE_HID,
+    fields=[
+        LE16(name='bcdHID', value=0x0110),
+        UInt8(name='bCountryCode', value=0x00),
+        UInt8(name='bNumDescriptors', value=0x01),
+        UInt8(name='bReportDescriptorType', value=_DescriptorTypes.REPORT, fuzzable=True),
+        LE16(name='wDescriptorLength', value=0x002f),  # should be changed ??
+    ])
+
 # Crashing windows
 # s_initialize('interface_descriptor')
 # s_sizer(name='bLength', block_name='descriptor_block', length=1, fuzzable=False, inclusive=True)  # 9 Bytes
 # if s_block_start('descriptor_block'):
-#     s_byte(name='bDesciptorType', value=_DescriptorTypes.INTERFACE, fuzzable=False)
+#     UInt8(name='bDescriptorType', value=_DescriptorTypes.INTERFACE, fuzzable=True),
 #     s_byte(name='bInterfaceNumber', value=0, fuzzable=False)
 #     s_byte(name='bAlternateSetting', value=0, fuzzable=False)
 #     s_byte(name='bNumEndpoints', value=0, fuzzable=False)
